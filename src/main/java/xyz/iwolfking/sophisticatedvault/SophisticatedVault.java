a1faf0071ced55c0d64948a58bd6dede35e35837
iwolfking <thegreatiwk@gmail.com>
2025-10-29 16:33:26 -0500

    Add datagenerated wood chest and barrel recipes

--- src/main/java/xyz/iwolfking/sophisticatedvault/SophisticatedVault.java
+++ src/main/java/xyz/iwolfking/sophisticatedvault/SophisticatedVault.java
@@ -6,6 +6,7 @@ import net.minecraft.Util;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -105,5 +106,9 @@ public class SophisticatedVault {
                 }
             });
         }
+    }
+
+    public static ResourceLocation id(String id) {
+        return new ResourceLocation(SophisticatedVault.MODID, id);
     }
 }
--- src/main/java/xyz/iwolfking/sophisticatedvault/datagen/ModRecipeProvider.java
+++ src/main/java/xyz/iwolfking/sophisticatedvault/datagen/ModRecipeProvider.java
@@ -1,14 +1,28 @@
 package xyz.iwolfking.sophisticatedvault.datagen;
 
 import iskallia.vault.init.ModItems;
+import net.minecraft.data.BlockFamily;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.properties.WoodType;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.p3pp3rf1y.sophisticatedcore.crafting.ShapeBasedRecipeBuilder;
+import net.p3pp3rf1y.sophisticatedcore.crafting.ShapelessBasedRecipeBuilder;
+import net.p3pp3rf1y.sophisticatedcore.util.RegistryHelper;
+import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
+import net.p3pp3rf1y.sophisticatedstorage.block.WoodStorageBlockBase;
+import net.p3pp3rf1y.sophisticatedstorage.item.WoodStorageBlockItem;
 import xyz.iwolfking.sophisticatedvault.SophisticatedVault;
+import xyz.iwolfking.sophisticatedvault.data.VaultBlockFamilies;
+import xyz.iwolfking.sophisticatedvault.data.VaultWoodTypes;
 import xyz.iwolfking.sophisticatedvault.init.ModBlocks;
 
 import java.util.function.Consumer;
@@ -41,6 +55,19 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
         strongboxUpgrade(consumer, ModBlocks.SOPHISTICATED_VAULT_LIVING_STRONGBOX, ModBlocks.SOPHISTICATED_VAULT_LIVING_CHEST, ModItems.LIVING_CHEST_SCROLL, ModItems.EXTRAORDINARY_LARIMAR);
         strongboxUpgrade(consumer, ModBlocks.SOPHISTICATED_VAULT_ORNATE_STRONGBOX, ModBlocks.SOPHISTICATED_VAULT_ORNATE_CHEST, ModItems.ORNATE_CHEST_SCROLL, ModItems.EXTRAORDINARY_LARIMAR);
 
+        woodChestRecipe(consumer, VaultWoodTypes.CHROMATIC, VaultBlockFamilies.CHROMATIC.getBaseBlock());
+        woodChestRecipe(consumer, VaultWoodTypes.VAULT_PLANKS, VaultBlockFamilies.VAULT_PLANKS.getBaseBlock());
+        woodChestRecipe(consumer, VaultWoodTypes.DRIFTWOOD, VaultBlockFamilies.DRIFTWOOD.getBaseBlock());
+        woodChestRecipe(consumer, VaultWoodTypes.TENOS, VaultBlockFamilies.TENOS.getBaseBlock());
+        woodChestRecipe(consumer, VaultWoodTypes.VELARA, VaultBlockFamilies.VELARA.getBaseBlock());
+        woodChestRecipe(consumer, VaultWoodTypes.OVERGROWN, VaultBlockFamilies.OVERGROWN.getBaseBlock());
+        woodBarrelRecipe(consumer, VaultWoodTypes.CHROMATIC, VaultBlockFamilies.CHROMATIC.getBaseBlock(), VaultBlockFamilies.CHROMATIC.get(BlockFamily.Variant.SLAB));
+        woodBarrelRecipe(consumer, VaultWoodTypes.VAULT_PLANKS, VaultBlockFamilies.VAULT_PLANKS.getBaseBlock(),VaultBlockFamilies.VAULT_PLANKS.get(BlockFamily.Variant.SLAB));
+        woodBarrelRecipe(consumer, VaultWoodTypes.DRIFTWOOD, VaultBlockFamilies.DRIFTWOOD.getBaseBlock(),VaultBlockFamilies.DRIFTWOOD.get(BlockFamily.Variant.SLAB));
+        woodBarrelRecipe(consumer, VaultWoodTypes.TENOS, VaultBlockFamilies.TENOS.getBaseBlock(),VaultBlockFamilies.TENOS.get(BlockFamily.Variant.SLAB));
+        woodBarrelRecipe(consumer, VaultWoodTypes.VELARA, VaultBlockFamilies.VELARA.getBaseBlock(),VaultBlockFamilies.VELARA.get(BlockFamily.Variant.SLAB));
+        woodBarrelRecipe(consumer, VaultWoodTypes.OVERGROWN, VaultBlockFamilies.OVERGROWN.getBaseBlock(),VaultBlockFamilies.OVERGROWN.get(BlockFamily.Variant.SLAB));
+
     }
 
     private void sophisticatedVaultChestRecipe(Consumer<FinishedRecipe> consumer, ItemLike output, ItemLike scrollItem, ItemLike mainCost, ItemLike secondaryCost) {
@@ -65,6 +92,75 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
                 .pattern("IMI")
                 .unlockedBy("has_" + chestItem.asItem().getRegistryName().getPath(), has(chestItem))
                 .save(consumer);
+    }
+
+    private void woodChestRecipe(Consumer<FinishedRecipe> consumer, WoodType woodType, Block planks) {
+        ShapeBasedRecipeBuilder.shaped(WoodStorageBlockItem.setWoodType(new ItemStack(net.p3pp3rf1y.sophisticatedstorage.init.ModBlocks.CHEST_ITEM.get()), woodType))
+                .pattern("PPP")
+                .pattern("PRP")
+                .pattern("PPP")
+                .define('P', planks)
+                .define('R', Blocks.REDSTONE_TORCH)
+                .unlockedBy("has_" + woodType.name() + "_plank", has(planks))
+                .save(consumer, SophisticatedStorage.getRL(woodType.name() + "_chest"));
+    }
+
+    private void woodBarrelRecipe(Consumer<FinishedRecipe> consumer, WoodType woodType, Block planks, Block slab) {
+        ShapeBasedRecipeBuilder.shaped(WoodStorageBlockItem.setWoodType(new ItemStack(net.p3pp3rf1y.sophisticatedstorage.init.ModBlocks.BARREL_ITEM.get()), woodType))
+                .pattern("PSP")
+                .pattern("PRP")
+                .pattern("PSP")
+                .define('P', planks)
+                .define('S', slab)
+                .define('R', Blocks.REDSTONE_TORCH)
+                .unlockedBy("has_" + woodType.name() + "_plank", has(planks))
+                .save(consumer, SophisticatedStorage.getRL(woodType.name() + "_barrel"));
+        limitedWoodBarrel1Recipe(consumer, woodType, planks, slab);
+        limitedWoodBarrel2Recipe(consumer, woodType, planks, slab);
+        limitedWoodBarrel3Recipe(consumer, woodType, planks, slab);
+        limitedWoodBarrel4Recipe(consumer, woodType, planks, slab);
+    }
+
+    private void limitedWoodBarrelRecipe(Consumer<FinishedRecipe> consumer, WoodType woodType, Block planks, Block slab, Consumer<ShapeBasedRecipeBuilder> addPattern, BlockItem item) {
+        ShapeBasedRecipeBuilder builder = ShapeBasedRecipeBuilder.shaped(WoodStorageBlockItem.setWoodType(new ItemStack(item), woodType))
+                .define('P', planks)
+                .define('S', slab)
+                .define('R', Blocks.REDSTONE_TORCH)
+                .unlockedBy("has_" + woodType.name() + "_plank", has(planks));
+        addPattern.accept(builder);
+        builder.save(consumer, SophisticatedStorage.getRL(woodType.name() + "_" + RegistryHelper.getRegistryName(item).orElseThrow().getPath()));
+    }
+
+    private void limitedWoodBarrel1Recipe(Consumer<FinishedRecipe> consumer, WoodType woodType, Block planks, Block slab) {
+        limitedWoodBarrelRecipe(consumer, woodType, planks, slab, builder ->
+                        builder.pattern("PSP")
+                                .pattern("PRP")
+                                .pattern("PPP")
+                , net.p3pp3rf1y.sophisticatedstorage.init.ModBlocks.LIMITED_BARREL_1_ITEM.get());
+    }
+
+    private void limitedWoodBarrel2Recipe(Consumer<FinishedRecipe> consumer, WoodType woodType, Block planks, Block slab) {
+        limitedWoodBarrelRecipe(consumer, woodType, planks, slab, builder ->
+                        builder.pattern("PPP")
+                                .pattern("SRS")
+                                .pattern("PPP")
+                , net.p3pp3rf1y.sophisticatedstorage.init.ModBlocks.LIMITED_BARREL_2_ITEM.get());
+    }
+
+    private void limitedWoodBarrel3Recipe(Consumer<FinishedRecipe> consumer, WoodType woodType, Block planks, Block slab) {
+        limitedWoodBarrelRecipe(consumer, woodType, planks, slab, builder ->
+                        builder.pattern("PSP")
+                                .pattern("PRP")
+                                .pattern("SPS")
+                , net.p3pp3rf1y.sophisticatedstorage.init.ModBlocks.LIMITED_BARREL_3_ITEM.get());
+    }
+
+    private void limitedWoodBarrel4Recipe(Consumer<FinishedRecipe> consumer, WoodType woodType, Block planks, Block slab) {
+        limitedWoodBarrelRecipe(consumer, woodType, planks, slab, builder ->
+                        builder.pattern("SPS")
+                                .pattern("PRP")
+                                .pattern("SPS")
+                , net.p3pp3rf1y.sophisticatedstorage.init.ModBlocks.LIMITED_BARREL_4_ITEM.get());
     }
 
     /**
